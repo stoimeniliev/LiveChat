18d822b983efdfcf2cd315cab62a2b880f2a5cb6
Petar Metodiev <petar.metodiev@gmail.com>
2016-09-22 02:46:57 +0300

    Minor changes.

--- Source/LiveChat/Client/LiveChat.Client/app/app.component.ts
+++ Source/LiveChat/Client/LiveChat.Client/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 @Component({
-  selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
-	})
-	export class AppComponent { }
+	selector: 'my-app',
+	template: '<h1>My First Editted Angular App</h1>'
+})
+
+export class AppComponent { }
