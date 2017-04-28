d39eac5f89172144cc650be8d56256be2d1d5ea6
Petar Metodiev <petar.metodiev@gmail.com>
2017-04-29 01:03:51 +0300

    Add some test components from angular material

--- Source/LiveChat/Client/LiveChat.Client/src/app/app.module.ts
+++ Source/LiveChat/Client/LiveChat.Client/src/app/app.module.ts
@@ -2,21 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MdSidenavModule,
+  MdButtonModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import 'hammerjs'
+import 'hammerjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
+	BrowserModule,
+	FormsModule,
+	HttpModule,
+	BrowserAnimationsModule,
+	MdSidenavModule,
+	MdButtonModule,
+	AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
