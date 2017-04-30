ab1c46fc99027b22ba03ec4e812b5d1ffd93ebf9
Petar Metodiev <petar.metodiev@gmail.com>
2017-05-01 00:38:36 +0300

    Toggle sidebar only on mobile

--- Source/LiveChat/Client/LiveChat.Client/src/app/app.component.ts
+++ Source/LiveChat/Client/LiveChat.Client/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'intrct-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'intrct-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'intrct works!';
+	isMobile: boolean = window.matchMedia("only screen and (max-width: 760px)").matches;
+	opened: boolean = !this.isMobile;
+	mode: string = this.isMobile ? 'over' : 'side';
+
+	constructor(){
+		console.log(this.isMobile);
+	}
+
+	openSidebar(sidebar){
+		sidebar.open();
+	}
 }
