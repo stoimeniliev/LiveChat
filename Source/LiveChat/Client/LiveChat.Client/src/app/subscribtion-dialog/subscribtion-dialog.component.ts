5dfdec80a585e6cf00a2a9df2ef93af4ad369e34
Petar Metodiev <petar.metodiev@gmail.com>
2017-05-04 22:41:38 +0300

    Add template for subscribtion modal in the home page

---
+++ Source/LiveChat/Client/LiveChat.Client/src/app/subscribtion-dialog/subscribtion-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'intrct-subscribtion-dialog',
+	templateUrl: './subscribtion-dialog.component.html',
+	styleUrls: ['./subscribtion-dialog.component.scss']
+})
+export class SubscribtionDialogComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
