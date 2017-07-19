dc4c696d89a9ba37a62be8794a34896d0b28e7c4
Petar Metodiev <petar.metodiev@gmail.com>
2017-07-19 22:07:07 +0300

    Formatting

--- Source/LiveChat/Client/LiveChat.Client/src/app/projector/projector.component.ts
+++ Source/LiveChat/Client/LiveChat.Client/src/app/projector/projector.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'intrct-projector',
-  templateUrl: './projector.component.html',
-  styleUrls: ['./projector.component.scss']
+	selector: 'intrct-projector',
+	templateUrl: './projector.component.html',
+	styleUrls: ['./projector.component.scss']
 })
 export class ProjectorComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
